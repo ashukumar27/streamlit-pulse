fa1cf4d1a9b3b7f40068ebf662661f3982544197
Ashutosh Kumar <ashu.iitkgp@gmail.com>
2021-11-30 15:32:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,18 +5,46 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 
+
+
+# loading in all the essentials for data manipulation
+import pandas as pd
+import numpy as np
+#load inthe NTLK stopwords to remove articles, preposition and other words that are not actionable
+from nltk.corpus import stopwords
+# This allows to create individual objects from a bog of words
+from nltk.tokenize import word_tokenize
+# Lemmatizer helps to reduce words to the base form
+from nltk.stem import WordNetLemmatizer
+# Ngrams allows to group words in common pairs or trigrams..etc
+from nltk import ngrams
+# We can use counter to count the objects
+from collections import Counter
+# This is our visual library
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+
+
+
 import numpy as np
 
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.write("""
 # Pulse Survey Results
-Please note: sentiments may not be 100 percent accurate	""")
+Please note: sentiments may not be 100 percent accurate   
+Hover mouse on the row to read the entire review.
+	""")
 
-
+stopwords = ['more','better','a','the','to','and','for','of','that','are','i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']
 
 df = pd.read_csv('./df_cleaned_v1.csv')
 
+
+df.drop(['field1'], axis=1, inplace=True)
 #st.write(df['field1_Cleaned1'].head())
 
 
@@ -27,6 +55,7 @@ def generate_wordcloud(data, title, mask=None,colormap='RdYlGn'):
                       colormap=color_map,
                       mask=None,
                       background_color='white',
+                      stopwords=stopwords,
                       collocations=True).generate_from_text(data)
     plt.figure(figsize=(10,8))
     plt.imshow(cloud)
@@ -34,6 +63,8 @@ def generate_wordcloud(data, title, mask=None,colormap='RdYlGn'):
     plt.title(title)
     plt.show()
     st.pyplot()
+
+
 
 
 ### Sidebar Configuration
@@ -69,7 +100,7 @@ else:
 
 
 st.write(doc_string)
-st.write(df_select2.head(20))
+st.write(df_select2['field1_Cleaned1'].head(50))
 
 
 
@@ -88,3 +119,43 @@ color_map = st.selectbox(
 
 
 generate_wordcloud(long_string, 'WordCloud', mask=None)
+
+
+
+
+def word_frequency(sentence):
+	# joins all the sentenses
+	#sentence =" ".join(sentence)
+	# creates tokens, creates lower class, removes numbers and lemmatizes the words
+	new_tokens = word_tokenize(sentence)
+	new_tokens = [t.lower() for t in new_tokens]
+	new_tokens =[t for t in new_tokens if t not in stopwords]
+	new_tokens = [t for t in new_tokens if t.isalpha()]
+	lemmatizer = WordNetLemmatizer()
+	new_tokens =[lemmatizer.lemmatize(t) for t in new_tokens]
+	#counts the words, pairs and trigrams
+	counted = Counter(new_tokens)
+	counted_2= Counter(ngrams(new_tokens,2))
+	counted_3= Counter(ngrams(new_tokens,3))
+	#creates 3 data frames and returns thems
+	word_freq = pd.DataFrame(counted.items(),columns=['word','frequency']).sort_values(by='frequency',ascending=False)
+	word_pairs =pd.DataFrame(counted_2.items(),columns=['pairs','frequency']).sort_values(by='frequency',ascending=False)
+	trigrams =pd.DataFrame(counted_3.items(),columns=['trigrams','frequency']).sort_values(by='frequency',ascending=False)
+	return word_freq,word_pairs,trigrams
+
+
+word_freq,word_pairs,trigrams = word_frequency(long_string)
+
+
+word_freq_20 = word_freq[0:19]
+word_pairs_20 = word_pairs[0:19]
+trigrams_20 = trigrams[0:19]
+
+
+## Plot 
+
+fig, axes = plt.subplots(3,1,figsize=(8,20))
+sns.barplot(ax=axes[0],x='frequency',y='word',data=word_freq_20)
+sns.barplot(ax=axes[1],x='frequency',y='pairs',data=word_pairs_20)
+sns.barplot(ax=axes[2],x='frequency',y='trigrams',data=trigrams_20)
+st.pyplot(fig)
